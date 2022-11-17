32da08cdfd95d2dfb7f1ebacfba4990a18270869
bimlendu33 <33177291+bimlendu33@users.noreply.github.com>
2022-11-18 00:12:02 +0530

    1 file modified

--- index.js
+++ index.js
@@ -7,18 +7,31 @@ var obj = {
     k: null,
   },
 };
+/* I shall use single array, this will keep on updating on latest path (key) value */
 var valueOfPath = [];
 const findPath = (object, path) => {
+  /* Create an array from path (keys) which are coming with dot separation */
   var pathsList = path.split('.');
+
+  /* check for if first path (key) is existing else break condition and return undefined*/
   if (valueOfPath.indexOf(pathsList[0]) < 0 && object[pathsList[0]]) {
+    /*
+     update Array with splice now with first path (key) value so that we can check for next path(key) 
+    in the same Array
+    */
     valueOfPath.splice(0, valueOfPath.length, object[pathsList[0]]);
+
+    /* Now iterate over left paths (keys) and check if their key-value exists in Array*/
     for (var i = 1; i < pathsList.length; i++) {
       if (valueOfPath[0][pathsList[i]] != undefined) {
+        /*Validate the next path is not undefined else handle that in else and break the loop */
         valueOfPath.splice(0, valueOfPath.length, valueOfPath[0][pathsList[i]]);
       } else {
+        /* null should be returned explicitly */
         if (valueOfPath[0][pathsList[i]] === null) {
           valueOfPath.splice(0, valueOfPath.length, null);
         } else {
+          /* if path (key) is not exisiting then simply return undefined and break the loop */
           valueOfPath.splice(0, valueOfPath.length, undefined);
         }
         break;
