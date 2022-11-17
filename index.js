53a0e0ca064716c179fce18584a3aabd1e5dfa43
bimlendu33 <33177291+bimlendu33@users.noreply.github.com>
2022-11-17 23:43:45 +0530

    1 file modified

--- index.js
+++ index.js
@@ -9,27 +9,25 @@ var obj = {
 };
 var valueOfPath = [];
 const findPath = (object, path) => {
-  var pathArr = path.split('.');
-  if (valueOfPath.indexOf(pathArr[0]) < 0) {
-    if (object[pathArr[0]]) {
-      valueOfPath.splice(0, valueOfPath.length, object[pathArr[0]]);
-      for (var i = 1; i < pathArr.length; i++) {
-        if (valueOfPath[0][pathArr[i]] != undefined) {
-          valueOfPath.splice(0, valueOfPath.length, valueOfPath[0][pathArr[i]]);
+  var pathsList = path.split('.');
+  if (valueOfPath.indexOf(pathsList[0]) < 0 && object[pathsList[0]]) {
+    valueOfPath.splice(0, valueOfPath.length, object[pathsList[0]]);
+    for (var i = 1; i < pathsList.length; i++) {
+      if (valueOfPath[0][pathsList[i]] != undefined) {
+        valueOfPath.splice(0, valueOfPath.length, valueOfPath[0][pathsList[i]]);
+      } else {
+        if (valueOfPath[0][pathsList[i]] === null) {
+          valueOfPath.splice(0, valueOfPath.length, null);
         } else {
-          if (valueOfPath[0][pathArr[i]] === null) {
-            valueOfPath.splice(0, valueOfPath.length, null);
-          } else {
-            valueOfPath.splice(0, valueOfPath.length, undefined);
-          }
-          break;
+          valueOfPath.splice(0, valueOfPath.length, undefined);
         }
+        break;
       }
     }
   } else {
     valueOfPath.splice(0, valueOfPath.length, undefined);
   }
-  for (var i = 0; i < pathArr.length; i++) {}
+  for (var i = 0; i < pathsList.length; i++) {}
 
   return valueOfPath[0];
 };
